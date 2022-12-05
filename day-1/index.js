5013c700e76e397b9bcc2af7cfd5b3ea3e6a4c18
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2022-12-05 23:51:12 +0000

    day 1, part 2

--- day-1/index.js
+++ day-1/index.js
@@ -8,7 +8,7 @@ const inputData = fs.readFileSync("inputdata.txt", "utf8");
 const splitElves = inputData.split(/^\n+$/m); // Split on empty line
 console.log("Count of elves: ", splitElves.length);
 
-let largestCalories = 0;
+let largestCalories = [];
 
 for (let index = 0; index < splitElves.length; index++) {
   const elfsCalories = splitElves[index];
@@ -16,22 +16,40 @@ for (let index = 0; index < splitElves.length; index++) {
   if (elfsCalories.length > 0) {
     const chunkOfCalories = elfsCalories.split(/[\r|\n]+/); // Split on new line
 
-    const totalCalories = chunkOfCalories.reduce((accumulator, currentValue) => {
-      // Test line only contains numbers
-      if (/[0-9]+/.test(currentValue)) {
-        return accumulator + Number(currentValue);
-      }
-      return accumulator;
-    }, 0);
+    const totalCalories = chunkOfCalories.reduce(
+      (accumulator, currentValue) => {
+        // Test line only contains numbers
+        if (/[0-9]+/.test(currentValue)) {
+          return accumulator + Number(currentValue);
+        }
+        return accumulator;
+      },
+      0
+    );
 
     console.log(
       `Elf: ${index}, Items: ${elfsCalories.length}, Total Calories: ${totalCalories}`
     );
 
-    if (totalCalories > largestCalories) {
-      largestCalories = totalCalories;
-    }
+    largestCalories.push(totalCalories);
   }
 }
 
-console.log(`Largest elf's calories are: ${largestCalories}`);
+largestCalories.sort((a, b) => b - a);
+
+console.log("Largest elf's calories are: ", largestCalories[0]);
+
+let topThree = [];
+
+for (let index = 0; index < 3; index++) {
+  const calories = largestCalories[index];
+
+  topThree.push(calories);
+}
+
+console.log("The top three calories are: ", topThree);
+
+console.log(
+  "The total for the top three elves is: ",
+  topThree.reduce((a, b) => a + b, 0)
+);
