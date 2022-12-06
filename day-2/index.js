4bfc07b7ff4eb67aa79ccfb67baf98445dff601e
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2022-12-06 21:57:07 +0000

    day 2, part 2

--- day-2/index.js
+++ day-2/index.js
@@ -63,6 +63,61 @@ function getRoundWinner(player1Choice, player2Choice) {
   throw new Error("Missing a case");
 }
 
+/** @param {string} choice */
+function getPt2RoundWinner(choice) {
+  switch (choice) {
+    case "X":
+      return {
+        player1Outcome: "win",
+        player2Outcome: "loose",
+      };
+
+    case "Y":
+      return {
+        player1Outcome: "draw",
+        player2Outcome: "draw",
+      };
+
+    case "Z":
+      return {
+        player1Outcome: "loose",
+        player2Outcome: "win",
+      };
+  }
+
+  throw new Error("Missing a case");
+}
+
+/**
+ * @param {keyof shapeScore} player1Shape
+ * @param {keyof roundOutcome} player2Outcome
+ * @returns {keyof shapeScore}
+ */
+function getPt2Player2Shape(player1Shape, player2Outcome) {
+  if (player2Outcome === "draw") {
+    return player1Shape;
+  }
+
+  switch (`${player1Shape}_${player2Outcome}`) {
+    case "rock_win":
+      return "paper";
+    case "rock_loose":
+      return "scissors";
+
+    case "paper_win":
+      return "scissors";
+    case "paper_loose":
+      return "rock";
+
+    case "scissors_win":
+      return "rock";
+    case "scissors_loose":
+      return "paper";
+  }
+
+  throw new Error("Missing a case");
+}
+
 /**
  * @param {keyof shapeScore} _selectedShape
  * @param {keyof roundOutcome} _roundOutcome
@@ -90,12 +145,16 @@ for (let index = 0; index < roundData.length; index++) {
     const [player1, player2] = round.split(" ");
 
     const player1Shape = mapInputToShape(player1);
-    const player2Shape = mapInputToShape(player2);
+    // const player2Shape = mapInputToShape(player2);
 
-    const { player1Outcome, player2Outcome } = getRoundWinner(
-      player1Shape,
-      player2Shape
-    );
+    // const { player1Outcome, player2Outcome } = getRoundWinner(
+    //   player1Shape,
+    //   player2Shape
+    // );
+
+    const { player1Outcome, player2Outcome } = getPt2RoundWinner(player2);
+
+    const player2Shape = getPt2Player2Shape(player1Shape, player2Outcome);
 
     const player1Score = getScore(player1Shape, player1Outcome);
     const player2Score = getScore(player2Shape, player2Outcome);
